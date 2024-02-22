b1f1183d169b1ebfc5d053407c808118063f7681
Kevin <kevinjcord@gmail.com>
2024-02-21 20:42:47 -0600

    added user login

--- Coding Project/server/routes/auth.js
+++ Coding Project/server/routes/auth.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { Register } from "../controllers/auth.js"
+import { Register, Login } from "../controllers/auth.js"
 import Validate from "../middleware/validate.js"
 import { check } from "express-validator"
 const router = express.Router();
@@ -28,6 +28,17 @@ router.post(
         .withMessage("Must be at least 8 chars long"),
     Validate,
     Register
+)
+
+router.post(
+    "/login",
+    check("email")
+        .isEmail()
+        .withMessage("Enter a valid email address")
+        .normalizeEmail(),
+    check("password").not().isEmpty(),
+    Validate,
+    Login
 )
 
 export { router }
