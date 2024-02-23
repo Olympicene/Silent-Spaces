e19a25036a01342932c6b298d4d71aff0654ec8f
Nandana <nandanasheri@gmail.com>
2024-02-22 19:46:37 -0600

    added laptop image to landing page

--- Coding Project/client/src/components/LandingPage.js
+++ Coding Project/client/src/components/LandingPage.js
@@ -4,6 +4,7 @@ import laptop from "../assets/laptopClipArt.svg";
 
 const LandingPage = () => {
   return (
+
     <div className='landing-page'>
       <div className='left-half'>
         <div className='div-title' style={{marginTop : "6rem"}}>
@@ -25,13 +26,70 @@ const LandingPage = () => {
           <button className='landing-button'>LOGIN</button>
           <button className='landing-button'>CREATE ACCOUNT</button>
         </div>
-      
       </div>
-      {/* <div className='image-container'>
-        <img className = "laptop-img" src={laptop} alt="" />
-        <img className = "laptop-img" src={laptop} alt="" />
-        <img className = "laptop-img" src={laptop} alt="" />
-      </div> */}
+
+      <div className="right-half">
+
+        <div className='gallery'>
+
+          <div className='gallery__item gallery__item--1'>
+            <figure>
+              <img  src={laptop} class="laptop-img" style = {{transform: 'rotate(-70deg)', paddingBottom: '4rem'}} alt=""/>
+            </figure>
+          </div>
+          
+          <div className='gallery__item gallery__item--2'>
+            <figure>
+              <img  src={laptop} class="laptop-img" style = {{transform: 'rotate(180deg)', paddingBottom: '0.5rem'}} alt=""/>
+            </figure>
+          </div>
+
+          <div className='gallery__item gallery__item--3'>
+            <figure>
+              <img  src={laptop} class="laptop-img" style = {{transform: 'rotate(30deg)'}} alt=""/>
+            </figure>
+          </div>
+
+          <div className='gallery__item gallery__item--4'>
+            <figure>
+              <img  src={laptop} class="laptop-img" style = {{transform: 'rotate(60deg)', paddingBottom: '2rem'}} alt=""/>
+            </figure>
+          </div>
+
+          <div className='gallery__item gallery__item--5'>
+            <figure>
+              <img  src={laptop} class="laptop-img" style = {{transform: 'rotate(-30deg)', paddingBottom: '3rem'}} alt=""/>
+            </figure>
+          </div>
+
+          <div className='gallery__item gallery__item--6'>
+            <figure>
+              <img  src={laptop} class="laptop-img" style = {{transform: 'rotate(50deg)'}} alt=""/>
+            </figure>
+          </div>
+
+          <div className='gallery__item gallery__item--7'>
+            <figure>
+              <img  src={laptop} class="laptop-img" style = {{transform: 'rotate(-150deg)', paddingBottom: '3rem'}} alt=""/>
+            </figure>
+          </div>
+
+          <div className='gallery__item gallery__item--8'>
+            <figure>
+              <img  src={laptop} class="laptop-img" style = {{transform: 'rotate(70deg)'}} alt=""/>
+            </figure>
+          </div>
+
+          <div className='gallery__item gallery__item--9'>
+            <figure>
+              <img  src={laptop} class="laptop-img" style = {{transform: 'rotate(120deg)', paddingBottom: '4rem'}} alt=""/>
+            </figure>
+          </div>
+
+        </div>
+
+      </div>
+
     </div>
   );
 }
