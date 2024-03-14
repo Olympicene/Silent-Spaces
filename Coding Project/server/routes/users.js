49a1c3024b29da47e53e5c37862f3e34174f1342
annie <ayou4@uic.edu>
2024-03-14 15:46:37 -0500

    progress of fav space

--- Coding Project/server/routes/users.js
+++ Coding Project/server/routes/users.js
@@ -1,9 +1,18 @@
 import express from "express"
+import { check } from "express-validator"
+import { Verify } from "../middleware/verify.js";
 var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* POST new Favorite Space on the list*/
+/*router.post(
+  '/add-favorite-space',
+  Verify,
+  AddFavoriteSpace,
+);*/
 
 export { router }
