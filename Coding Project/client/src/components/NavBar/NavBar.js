fa5ded615dd4020a059dfd00432afd835cb0414a
kevin <kevinjcord@gmail.com>
2024-03-28 21:43:54 -0500

    some changes to navbar

--- Coding Project/client/src/components/NavBar/NavBar.js
+++ Coding Project/client/src/components/NavBar/NavBar.js
@@ -7,12 +7,12 @@ import './NavBar.css';
 
 function NavBar() {
   return (
-    <div>
+    <div >
       <IconContext.Provider value={{color:"#ffff", size:"2rem"}}>
       <nav className='nav-menu active'> 
         <ul className='nav-menu-items'>
-          <h1 style={{marginTop:"1rem"}}>SILENT</h1>
-          <h1>SPACES</h1>
+          <h1 style={{marginTop:"1rem", marginBottom:"-10px"}}>SILENT</h1>
+          <h1> SPACES</h1>
           {NavbarData.map((item, index) => {
             return (
               <li key = {index} className={item.className}>
@@ -24,7 +24,10 @@ function NavBar() {
             )
           })}
           <div className='profile-section'>
-            <FaIcons.FaUser style={{color:"black", size:"2rem", marginRight:"1rem"}}/>
+            <div style={{marginRight: "0.5rem", backgroundColor: "grey", display: "flex", borderRadius : "50%"}}>
+              <FaIcons.FaUser style={{color:"black", size:"2rem", margin:"0.5rem"}}/>
+            </div>
+      
             <div style={{display:"inline-block"}}>
               <p style={{fontSize:"1.2rem"}}>username</p>
               <p style={{fontSize:"1rem", color:"#595A5E"}}>email</p>
