071f0d0bc41c24607601b4adfb077bfa0c6d7e87
Nandana <nandanasheri@gmail.com>
2024-04-09 23:18:47 -0500

    added favorite icon to space tile

--- Coding Project/client/src/components/SpaceTile/SpaceTile.js
+++ Coding Project/client/src/components/SpaceTile/SpaceTile.js
@@ -10,8 +10,8 @@ const SpaceTile = ({details}) => {
     let nameToDisplay = ""
 
     const [isClick, setClick] = useState(false);
-    if (details.name.length > 22) {
-        nameToDisplay = details.name.slice(0,22) + "...";
+    if (details.name.length > 17) {
+        nameToDisplay = details.name.slice(0,17) + "..";
     }
     else {
         nameToDisplay = details.name;
@@ -20,17 +20,17 @@ const SpaceTile = ({details}) => {
     return (
         <div className="space-tile">
             <Link to = {details.path} style={{ textDecoration: 'none' }}>
-            {/* <Heart isClick={isClick} onClick={() => setClick(!isClick)} /> */}
             <div className="spacetile-image" >
                 <img src={(details.img === undefined ? studySpace : details.img)} alt = "" style={{width:"100%", objectFit: "cover"}}/>
             </div>
             <div style={{float:"left"}} >
                 <p style={{color: "black", fontSize:"1.5rem", marginBottom:"1rem", textOverflow: "ellipsis", whiteSpace:"nowrap"}}>{(details.name === undefined? "N/A" : nameToDisplay)}</p>
-                <p style={{color:"#595A5E", fontSize:"1rem", margin:"1rem", position: "absolute", bottom: 0, left: 0 }}>{details.miles} miles away</p>
+                <p style={{color:"#595A5E", fontSize:"1rem", margin:"1rem", position: "absolute", bottom: 0, left: 0}}>{details.miles} miles away</p>
             </div>
-            <div style={{position: "absolute", bottom: "-1rem", right: "0", marginRight:"1rem" }}>
+            <div style={{position: "absolute", bottom: "-1rem", right: "0", marginRight:"1rem"}}>
                 <img className="tile-rating" src={star} alt = ""/>
                 <p className="tile-rating" style={{color: "black", fontSize:"1.5rem"}}>{(details.rating === undefined? "N/A" : details.rating)}</p>
+                <Heart isClick={isClick} onClick={() => setClick(!isClick)} styles={{position:"absolute", left:0, top:"-3.5rem"}} />
             </div>
             </Link>
         </div>
