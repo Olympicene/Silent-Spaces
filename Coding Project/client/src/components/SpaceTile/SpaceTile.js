c93b22ae8ca4dd3c8b7daa1a45a477fa1ac99dcb
Kevin <kevinjcord@gmail.com>
2024-04-10 16:34:40 -0500

    changed spacetiles to flexboxes and hearts to top right

--- Coding Project/client/src/components/SpaceTile/SpaceTile.js
+++ Coding Project/client/src/components/SpaceTile/SpaceTile.js
@@ -19,19 +19,23 @@ const SpaceTile = ({details}) => {
     
     return (
         <div className="space-tile">
+            <Heart isClick={isClick} onClick={() => setClick(!isClick)} styles={{position:"absolute", right:"-12px", top:"-12px", }} />
+
             <Link to = {details.path} style={{ textDecoration: 'none' }}>
             <div className="spacetile-image" >
                 <img src={(details.img === undefined ? studySpace : details.img)} alt = "" style={{width:"100%", objectFit: "cover"}}/>
             </div>
-            <div style={{float:"left"}} >
-                <p style={{color: "black", fontSize:"1.5rem", marginBottom:"1rem", textOverflow: "ellipsis", whiteSpace:"nowrap"}}>{(details.name === undefined? "N/A" : nameToDisplay)}</p>
-                <p style={{color:"#595A5E", fontSize:"1rem", margin:"1rem", position: "absolute", bottom: 0, left: 0}}>{details.miles} miles away</p>
+            <div className="spacetile-summary"> 
+                <div className="spacetile-info">
+                    <p style={{margin: 0, color: "black", fontSize:"1.5rem", textOverflow: "ellipsis",}}>{(details.name === undefined? "N/A" : nameToDisplay)}</p>
+                    <p style={{margin: 0, color:"black", fontSize:"1rem" }}>{details.miles} miles away</p>
+                </div>
+                <div className="spacetile-rating" >
+                    <img className="tile-rating" src={star} alt = ""/>
+                    <p className="tile-rating" style={{color: "black", fontSize:"1.5rem"}}>{(details.rating === undefined? "N/A" : details.rating)}</p>
+                </div>
             </div>
-            <div style={{position: "absolute", bottom: "-1rem", right: "0", marginRight:"1rem"}}>
-                <img className="tile-rating" src={star} alt = ""/>
-                <p className="tile-rating" style={{color: "black", fontSize:"1.5rem"}}>{(details.rating === undefined? "N/A" : details.rating)}</p>
-                <Heart isClick={isClick} onClick={() => setClick(!isClick)} styles={{position:"absolute", left:0, top:"-3.5rem"}} />
-            </div>
+
             </Link>
         </div>
     );
