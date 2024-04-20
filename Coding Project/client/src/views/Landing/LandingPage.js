a20fe9340376573dbef2142ae87b09759c4e1ef5
kevin <kevinjcord@gmail.com>
2024-04-20 14:47:07 -0500

    revamped landing page

--- Coding Project/client/src/views/Landing/LandingPage.js
+++ Coding Project/client/src/views/Landing/LandingPage.js
@@ -3,41 +3,31 @@ import { Link } from "react-router-dom";
 
 import Button from '../../components/Button/Button';
 import styles from './Landing.module.css'
+import logo from '../../assets/logo_white2.svg'
 
 const LandingPage = () => {
   return (
 
-    <div className={styles['landing-page']}>
+    <div className={styles['main']}>
       <div className={styles['left-half']}>
 
         <div className={styles['logo']}>
-          <div className={styles['logo-left']}>
-            <h1 className={styles['land-title']} style={{color: "white"}}>SIL</h1>
-            <h1 className={styles['land-title']} style={{color: "white"}}>SPA</h1>
-            <h1 className={styles['land-title']} style={{color: "white"}}>LOC</h1>
-          </div>
-
-          <div className={styles['logo-right']}>
-            <h1 className={styles['land-title']} style={{color: "#181D27"}}>ENT</h1>
-            <h1 className={styles['land-title']} style={{color: "#181D27"}}>CES</h1>
-            <h1 className={styles['land-title']} style={{color: "#181D27"}}>ATOR</h1>
-          </div>
+          <img className={styles['title']} alt="logo" src={logo}/>
         </div>
 
-        <div className={styles['button-div']} >
-        <Link to="/log-in">
-          <Button theme="light" style={{width: "80%"}}>LOG IN</Button>
-        </Link>
+        <div className={styles['CTA-buttons']} >
+          <Link to="/log-in">
+            <Button theme="light" style={{ width: "100%" }}>LOG IN</Button>
+          </Link>
 
-        <Link to="/sign-up">
-          <Button theme="dark" style={{width: "80%"}}>CREATE ACCOUNT</Button>
-        </Link>  
+          <Link to="/sign-up">
+            <Button theme="dark" style={{ width: "100%" }}>CREATE ACCOUNT</Button>
+          </Link>
         </div>
+
       </div>
 
-      <div className={styles['landing-right-half']}>
-      </div>
-
+      <div className={styles['right-half']}></div>
     </div>
   );
 }
