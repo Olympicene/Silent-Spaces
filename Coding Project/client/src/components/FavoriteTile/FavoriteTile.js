3c2314b5468aca037147935ddde88851ed16d52e
Nandana <nandanasheri@gmail.com>
2024-04-22 01:23:55 -0500

    added favorite spaces

--- Coding Project/client/src/components/FavoriteTile/FavoriteTile.js
+++ Coding Project/client/src/components/FavoriteTile/FavoriteTile.js
@@ -9,96 +9,82 @@ import { MdOutlinePersonOff } from "react-icons/md";
 import { MdOutlinePeople } from "react-icons/md";
 import { PiWifiSlashBold } from "react-icons/pi";
 import { PiWifiHighBold } from "react-icons/pi";
-import { IoShareSocialOutline } from "react-icons/io5";
 import { IconContext } from 'react-icons';
 import Rating from '@mui/material/Rating';
 import styles from "./FavoriteTile.module.css"
+import Tag from '../../components/Tag/Tag';
 
 
 
-const FavoriteTile = ({details, path}) => {
+const FavoriteTile = ({data, path}) => {
 
-    const [value, setValue] = React.useState(2);
-    const [hover, setHover] = React.useState(-1);
-
-    const Wifilabels = {
-        0.5: 'Useless',
-        1: 'Nearly Useless',
-        1.5: 'Very Slow',
-        2: 'Slow',
-        2.5: 'Usable',
-        3: 'Decent',
-        3.5: 'Good',
-        4: 'Very Good',
-        4.5: 'Fast',
-        5: 'Very Fast',
-      };
-
-    function getLabelText() {
-        return `${value} Star${value !== 1 ? 's' : ''}, ${Wifilabels[value]}`;
+    let nameToDisplay = ""
+    if (data.name.length > 30) {
+        nameToDisplay = data.name.slice(0,30) + "..";
     }
+    else {
+        nameToDisplay = data.name;
+    }
+
     return(
         <Link to = {path} style={{ textDecoration: 'none' }}>
+        <IconContext.Provider value={{size:"1.5rem"}}>
         <div className={styles['favoritetile-main']}>
             <div className={styles['favtile-img']}>
-                <img src={spaceimg} alt = ""/>
+                <img src={data.img[0]} alt = "" style={{width:"100%", objectFit: "cover"}}/>
                 <Heart isClick={true} styles={{position:"absolute", right:"-25px", top:"-30px", }} />
             </div>
 
             <div className={styles['favtile-middle']}>
-                <h2>sample study space</h2>
+                <div>
+                    <h2>{nameToDisplay}</h2>
+                </div>
                 <div className={styles['favtile-location']}>
                     <FaLocationDot className={styles['favtile-loc-icon']} />
-                    <h3>410 s morgan st</h3>
+                    <h3>{data.address}</h3>
                 </div>
                 <div className={styles['favtile-rating']}>
-                    <h2>4.3</h2>
+                    <h2>{data.rating}</h2>
                 </div>
             </div>
             
             <div className={styles['favtile-stats']}>
                 <div className={styles['favtile-stats-tile']}>
-                    <div>
-                        <FaVolumeHigh style={{marginRight: "20px", color: "black"}}/>
-                        <Rating name="size-large" readOnly defaultValue={2} sx={{
-                            fontSize: "2rem",
-                            '& .MuiRating-iconFilled': {
-                                color: 'black',
-                            },
-                        }} />  
-                        <FaVolumeMute style={{marginLeft: "20px", color: "black"}}/>
-                    </div>
+                    <FaVolumeHigh style={{marginLeft: "20px", color: "black", height: "3rem"}}/>
+                    <Rating name="size-large" readOnly value={data.statistics.noiseLevels} sx={{
+                        fontSize: "2rem",
+                        '& .MuiRating-iconFilled': {
+                            color: 'black',
+                        },
+                    }} />  
+                    <FaVolumeMute style={{marginRight: "20px", color: "black", height: "3rem"}}/>
                 </div>
 
                 <div className={styles['favtile-stats-tile']}>
-                    <div>
-                        <MdOutlinePeople style={{marginRight: "20px", color: "black", height: "3rem"}}/>
-                        <Rating readOnly value={2} sx={{
-                            fontSize: "2rem",
-                            '& .MuiRating-iconFilled': {
-                                color: 'black',
-                            },
-                        }} />
-                        <MdOutlinePersonOff style={{marginLeft: "20px", color: "black", height: "3rem"}}/>
-                    </div>
+                    <MdOutlinePeople style={{marginLeft: "20px", color: "black", height: "3rem"}}/>
+                    <Rating readOnly value={data.statistics.occupancy} sx={{
+                        fontSize: "2rem",
+                        '& .MuiRating-iconFilled': {
+                            color: 'black',
+                        },
+                    }} />
+                    <MdOutlinePersonOff style={{marginRight: "20px", color: "black", height: "3rem"}}/>
                 </div>
 
                 <div className={styles['favtile-stats-tile']}>
-                    <div>
-                        <PiWifiSlashBold style={{marginRight: "20px", color: "black", height: "3rem"}}/>
-                        <Rating readOnly value={2} sx={{
-                            fontSize: "2rem",
-                            '& .MuiRating-iconFilled': {
-                                color: 'black',
-                            },
-                        }} />
-                        <PiWifiHighBold style={{marginLeft: "20px", color: "black", height: "3rem"}}/>
-                    </div>
+                    <PiWifiSlashBold style={{marginLeft: "20px", color: "black", height: "3rem"}}/>
+                    <Rating readOnly value={data.statistics.connectivity} sx={{
+                        fontSize: "2rem",
+                        '& .MuiRating-iconFilled': {
+                            color: 'black',
+                        },
+                    }} />
+                    <PiWifiHighBold style={{marginRight: "20px", color: "black", height: "3rem"}}/>
                 </div>
-
             </div>
         
          </div>
+         </IconContext.Provider>
         </Link>
     );
 }
--- Coding Project/client/src/views/Favorites/FavoritesPage.js
+++ Coding Project/client/src/views/Favorites/FavoritesPage.js
@@ -1,8 +1,12 @@
 import FavoriteTile from '../../components/FavoriteTile/FavoriteTile';
 import NavBar from '../../components/NavBar/NavBar';
+import styles from './FavoritesPage.module.css';
+import React from 'react';
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
 
 const FavoritesPage = () => {
-    require('./FavoritesPage.css');
+    const navigate = useNavigate();
 
     const dummyuser = {
         first_name: 'naan',
@@ -10,12 +14,43 @@ const FavoritesPage = () => {
         email: 'urmom@gmail.com',
     }
 
+    const [favData, setFavData] = useState([]);
+
+    const getFavSpaces = async (endpoint) => {
+        try {
+            const response = await fetch(endpoint, {
+                method: 'GET',
+                credentials: 'include',
+            });
+
+            if (!response.ok) {
+                throw new Error('Auth failed');
+            }
+
+            const res = await response.json();
+            console.log(res.data[0])
+            const updatedFavData = res.data;
+
+            setFavData(updatedFavData);
+
+        } catch (error) {
+            console.error(error);
+            navigate('/log-in');
+        }
+    };
+
+    useEffect(() => {
+        getFavSpaces('http://localhost:5005/user/fav-space/all');
+    }, []);
+    console.log(favData);
     return (
-        <div className='favorites-main'>
+        <div className={styles['favorites-main']}>
             <NavBar info={dummyuser} page="favorites" />
-            <div className='favorites-right'>
-                <h1 className="favorites-title">FAVORITE SPACES</h1>
-                <FavoriteTile/>
+            <div className={styles['favorites-right']}>
+                <h1 className={styles['favorites-title']}>FAVORITE SPACES</h1>
+                {favData && favData.map((item, index) => (
+                    <FavoriteTile path={`/spaces/${item.id}`} key={index} data={item} /> 
+                ))}
             </div>
         </div>
     );
